5a7b5c0ca16569d34286bd28b06232f978121f1f
Adam Pigg <adam@piggz.co.uk>
2025-10-19 13:25:25 +0100

    Improve the configfs setup script

--- sparse/usr/bin/droid/setup-configfs.sh
+++ sparse/usr/bin/droid/setup-configfs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Set up configfs as we are not using droid-boot init-script
-
-USB_FUNCTIONS=ncm,mtp
+USB_DRIVER=ncm
+USB_FUNCTIONS=$USB_DRIVER,mtp
 GADGET_DIR=/sys/kernel/config/usb_gadget
 
 # Sugar for accessing usb config
@@ -18,7 +18,7 @@ write $GADGET_DIR/g1/strings/0x409/serialnumber "$1"
 write $GADGET_DIR/g1/strings/0x409/manufacturer "Halium"
 write $GADGET_DIR/g1/strings/0x409/product      "Halium Device"
 
-echo $USB_FUNCTIONS | grep -q "ncm"          && mkdir $GADGET_DIR/g1/functions/ncm.usb0
+echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && mkdir $GADGET_DIR/g1/functions/$USB_DRIVER.usb0
 echo $USB_FUNCTIONS | grep -q "mass_storage" && mkdir $GADGET_DIR/g1/functions/storage.0
 echo $USB_FUNCTIONS | grep -q "mtp"          && mkdir $GADGET_DIR/g1/functions/ffs.mtp
 
@@ -26,7 +26,7 @@ mkdir $GADGET_DIR/g1/configs/b.1
 mkdir $GADGET_DIR/g1/configs/b.1/strings/0x409
 write $GADGET_DIR/g1/configs/b.1/strings/0x409/configuration "$USB_FUNCTIONS"
 
-echo $USB_FUNCTIONS | grep -q "ncm"          && ln -s $GADGET_DIR/g1/functions/ncm.usb0 $GADGET_DIR/g1/configs/b.1
+echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && ln -s $GADGET_DIR/g1/functions/$USB_DRVER.usb0 $GADGET_DIR/g1/configs/b.1
 echo $USB_FUNCTIONS | grep -q "mass_storage" && ln -s $GADGET_DIR/g1/functions/storage.0 $GADGET_DIR/g1/configs/b.1
 echo $USB_FUNCTIONS | grep -q "mtp"          && ln -s $GADGET_DIR/g1/functions/ffs.mtp $GADGET_DIR/g1/configs/b.1
 
