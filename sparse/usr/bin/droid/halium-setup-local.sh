29ebc0b58ca5b3e50dc6e85c78c8b264de9063f8
Adam Pigg <adam@piggz.co.uk>
2025-10-20 20:10:30 +0100

    Dont modprobe the swpm modules early, do it after android is started.
    Loading these early causes bootloops, possibly due to some android side
    missing.  Load them after android startup.

--- sparse/usr/bin/droid/halium-setup-local.sh
+++ sparse/usr/bin/droid/halium-setup-local.sh
@@ -2,3 +2,8 @@
 
 mkdir -p /data/vendor/audiohal/audio_param/
 chmod a+w /data/vendor/audiohal/audio_param
+
+#Late load the swpm modules
+modprobe mtk-swpm
+modprobe mtk-swpm-dbg-common-v1
+modprobe mtk-swpm-dbg-v6789
