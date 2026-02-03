11d74bfd5c08770ae4727ece735e55a70edcb6df
Adam Pigg <adam@piggz.co.uk>
2026-02-03 11:33:56 +0000

    During updates, flash both A and B partitions and add lk flash

--- sparse/var/lib/platform-updates/flash-bootimg.sh
+++ sparse/var/lib/platform-updates/flash-bootimg.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 /usr/sbin/flash-partition boot_a /boot/boot.img
+/usr/sbin/flash-partition boot_b /boot/boot.img
+
 /usr/sbin/flash-partition dtbo_a /boot/dtbo.img
+/usr/sbin/flash-partition dtbo_b /boot/dtbo.img
+
 /usr/sbin/flash-partition vendor_boot_a /boot/vendor_boot.img
+/usr/sbin/flash-partition vendor_boot_b /boot/vendor_boot.img
+
+/usr/sbin/flash-partition lk_a lk.img
+/usr/sbin/flash-partition lk_b lk.img
