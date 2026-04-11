6d43e0b6507d5b921f29c215cb0bce20c809609c
Adam Pigg <adam@piggz.co.uk>
2026-04-11 19:23:05 +0100

    Fix typo

--- sparse/usr/bin/droid/setup-configfs.sh
+++ sparse/usr/bin/droid/setup-configfs.sh
@@ -26,7 +26,7 @@ mkdir $GADGET_DIR/g1/configs/b.1
 mkdir $GADGET_DIR/g1/configs/b.1/strings/0x409
 write $GADGET_DIR/g1/configs/b.1/strings/0x409/configuration "$USB_FUNCTIONS"
 
-echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && ln -s $GADGET_DIR/g1/functions/$USB_DRVER.usb0 $GADGET_DIR/g1/configs/b.1
+echo $USB_FUNCTIONS | grep -q "$USB_DRIVER"  && ln -s $GADGET_DIR/g1/functions/$USB_DRIVER.usb0 $GADGET_DIR/g1/configs/b.1
 echo $USB_FUNCTIONS | grep -q "mass_storage" && ln -s $GADGET_DIR/g1/functions/storage.0 $GADGET_DIR/g1/configs/b.1
 echo $USB_FUNCTIONS | grep -q "mtp"          && ln -s $GADGET_DIR/g1/functions/ffs.mtp $GADGET_DIR/g1/configs/b.1
 
